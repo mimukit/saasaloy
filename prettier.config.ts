dd50474db28130487962a50a0d28dff5ae174b4a
Mukitul Islam Mukit <hellomukit@gmail.com>
2026-01-14 21:26:08 +0600

    chore(prettier): enhance Prettier configuration and update dependencies
    This commit updates the Prettier configuration to include additional plugins for improved formatting and import sorting. The following changes were made:
    
    * Added `prettier-plugin-astro` and `prettier-plugin-tailwindcss` to the Prettier configuration.
    * Updated the `.prettierignore` file to exclude `.gitignore` and `.svg` files.
    * Added new dependencies for the plugins in `package.json` and updated the `pnpm-lock.yaml` file accordingly.
    * Adjusted the Prettier configuration to specify the order of plugins and added an override for Astro files.

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,12 +1,16 @@
 /** @type {import('prettier').Config} */
 export default {
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss", // needs to be last
+  ],
   endOfLine: "lf",
   semi: true,
   singleQuote: false,
   jsxSingleQuote: false,
   tabWidth: 2,
   trailingComma: "es5",
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
   importOrder: [
     "^react",
     "<BUILTIN_MODULES>",
@@ -16,4 +20,12 @@ export default {
   ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
