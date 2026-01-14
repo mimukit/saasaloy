b4d827e42b8085b4fb1e14e65fea598e3255f0da
Mukitul Islam Mukit <hellomukit@gmail.com>
2026-01-14 14:24:43 +0600

    chore(dependencies): add prettier & config
    This commit updates the package.json to include Prettier as a development dependency and introduces a new pnpm-lock.yaml file to manage package versions and ensure consistent installations across environments.

---
+++ prettier.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+export default {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
